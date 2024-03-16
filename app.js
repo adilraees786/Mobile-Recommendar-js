3e7b8a2b35b34b66c22b1a6e0716cb6d714cca55
adilraees786 <adilraees911@gmail.com>
2024-03-16 11:08:48 +0500

    Mobile Recommandar js

---
+++ app.js
@@ -0,0 +1,271 @@
+var budget=prompt("What is your Budget(low/medium/high)")
+
+var operater=prompt("Which operating system do you prefer?(Android/Ios)")
+
+var camera=prompt("What camera Quailty do you need? (basic/moderate/high)")
+
+var bettery=prompt("How important is battery life to you? (short/average/long)")
+
+
+var screen=prompt("What screen size do you prefer? (small/medium/large)")
+
+if(budget == "low" && operater == "Ios" && camera == "basic"&& bettery == "short" && screen == "small" ){
+    document.write("iphone: 7s", '<br>')
+
+    document.write("price: $80" , '<br>')
+
+    document.write("Operating System: ios" , '<br>')
+
+    document.write("camera:20MP " , '<br>')
+
+    document.write("Battery LIfe: 10 Hours" , '<br>')
+
+    document.write("Screen Size: 6.3 inches" , '<br>')
+}
+
+
+
+else if(budget == "low" && operater == "Ios" && camera == "moderate"&& bettery == "average" && screen == "large" ){
+    document.write("Iphone: 6s ", '<br>')
+
+    document.write("price: $300" , '<br>')
+
+    document.write("Operating System: Ios" , '<br>')
+
+    document.write("camera:50MP " , '<br>')
+
+    document.write("Battery LIfe: 15 Hours" , '<br>')
+
+    document.write("Screen Size: 5 inches" , '<br>')
+}
+
+
+
+
+
+else if(budget == "medium" && operater == "Ios" && camera == "high"&& bettery == "high" && screen == "large" ){
+    document.write("Iphone: 11 pro max", '<br>')
+
+    document.write("price: $1000" , '<br>')
+
+    document.write("Operating System: Ios" , '<br>')
+
+    document.write("camera:160MP " , '<br>')
+
+    document.write("Battery LIfe: 30 Hours" , '<br>')
+
+    document.write("Screen Size: 6 inches" , '<br>')
+}
+
+
+
+
+else if(budget == "high" && operater == "Ios" && camera == "moderate"&& bettery == "average" && screen == "large" ){
+    document.write("Iphone: X  max", '<br>')
+
+    document.write("price: $1300" , '<br>')
+
+    document.write("Operating System: Ios" , '<br>')
+
+    document.write("camera:150MP " , '<br>')
+
+    document.write("Battery LIfe: 35 Hours" , '<br>')
+
+    document.write("Screen Size: 7hi inches" , '<br>')
+}
+
+
+
+
+
+
+
+else if(budget == "medium" && operater == "Ios" && camera == "moderate"&& bettery == "average" && screen == "medium" ){
+    document.write("Iphone: 12 pro max", '<br>')
+
+    document.write("price: $1800" , '<br>')
+
+    document.write("Operating System: Ios" , '<br>')
+
+    document.write("camera:120MP " , '<br>')
+
+    document.write("Battery LIfe: 28 Hours" , '<br>')
+
+    document.write("Screen Size: 7.7 inches" , '<br>')
+}
+
+
+
+else if(budget == "high" && operater == "Ios" && camera == "high"&& bettery == "long" && screen == "large" ){
+    document.write("Iphone: 15 pro max", '<br>')
+
+    document.write("price: $8000" , '<br>')
+
+    document.write("Operating System: Ios" , '<br>')
+
+    document.write("camera:200MP " , '<br>')
+
+    document.write("Battery LIfe: 48 Hours" , '<br>')
+
+    document.write("Screen Size: 8.7 inches" , '<br>')
+}
+
+
+else if(budget == "low" && operater == "Android" && camera == "basic"&& bettery == "short" && screen == "small" ){
+    document.write("Tecno: Spark G0 2023", '<br>')
+
+    document.write("price: $400" , '<br>')
+
+    document.write("Operating System: Android" , '<br>')
+
+    document.write("camera:20MP " , '<br>')
+
+    document.write("Battery LIfe: 16 Hours" , '<br>')
+
+    document.write("Screen Size: 6.4 inches" , '<br>')
+}
+
+
+
+
+else if(budget == "high" && operater == "Android" && camera == "moderate"&& bettery == "average" && screen == "large" ){
+    document.write("Tecno: Spark 6", '<br>')
+
+    document.write("price: $400" , '<br>')
+
+    document.write("Operating System: Android" , '<br>')
+
+    document.write("camera:20MP " , '<br>')
+
+    document.write("Battery LIfe: 16 Hours" , '<br>')
+
+    document.write("Screen Size: 6.4 inches" , '<br>')
+}
+
+
+
+
+else if(budget == "low" && operater == "Android" && camera == "high"&& bettery == "average" && screen == "medium" ){
+    document.write("OPPO: note 6", '<br>')
+
+    document.write("price: $400" , '<br>')
+
+    document.write("Operating System: Android" , '<br>')
+
+    document.write("camera:20MP " , '<br>')
+
+    document.write("Battery LIfe: 16 Hours" , '<br>')
+
+    document.write("Screen Size: 6.4 inches" , '<br>')
+}
+
+
+
+
+
+else if(budget == "low" && operater == "Android" && camera == "high"&& bettery == "average" && screen == "large" ){
+    document.write("OPPO: note 6", '<br>')
+
+    document.write("price: $400" , '<br>')
+
+    document.write("Operating System: Android" , '<br>')
+
+    document.write("camera:20MP " , '<br>')
+
+    document.write("Battery LIfe: 16 Hours" , '<br>')
+
+    document.write("Screen Size: 6.4 inches" , '<br>')
+}
+
+
+
+
+
+else if(budget == "low" && operater == "Android" && camera == "moderate"&& bettery == "long" && screen == "large" ){
+    document.write("OPPO: note 6", '<br>')
+
+    document.write("price: $400" , '<br>')
+
+    document.write("Operating System: Android" , '<br>')
+
+    document.write("camera:20MP " , '<br>')
+
+    document.write("Battery LIfe: 16 Hours" , '<br>')
+
+    document.write("Screen Size: 6.4 inches" , '<br>')
+}
+
+
+
+
+else if(budget == "high" && operater == "Android" && camera == "basic"&& bettery == "short" && screen == "large" ){
+    document.write("vivo: y36", '<br>')
+
+    document.write("price: $400" , '<br>')
+
+    document.write("Operating System: Android" , '<br>')
+
+    document.write("camera:20MP " , '<br>')
+
+    document.write("Battery LIfe: 16 Hours" , '<br>')
+
+    document.write("Screen Size: 6.4 inches" , '<br>')
+}
+
+
+
+
+
+
+
+else if(budget == "high" && operater == "Android" && camera == "moderate"&& bettery == "average" && screen == "medium" ){
+    document.write("vivo: y97 ", '<br>')
+
+    document.write("price: $400" , '<br>')
+
+    document.write("Operating System: Android" , '<br>')
+
+    document.write("camera:20MP " , '<br>')
+
+    document.write("Battery LIfe: 16 Hours" , '<br>')
+
+    document.write("Screen Size: 6.4 inches" , '<br>')
+}
+
+
+
+else if(budget == "medium" && operater == "Android" && camera == "moderate"&& bettery == "average" && screen == "medium" ){
+    document.write("Svmsung Galaxy: Note 10", '<br>')
+
+    document.write("price: $800" , '<br>')
+
+    document.write("Operating System: Android" , '<br>')
+
+    document.write("camera:100MP " , '<br>')
+
+    document.write("Battery LIfe: 20 Hours" , '<br>')
+
+    document.write("Screen Size: 6.7 inches" , '<br>')
+}
+
+
+else if(budget == "high" && operater == "Android" && camera == "high"&& bettery == "long" && screen == "large" ){
+    document.write("Svmsung Galaxy: 24 Ultra", '<br>')
+
+    document.write("price: $4800" , '<br>')
+
+    document.write("Operating System: Android" , '<br>')
+
+    document.write("camera:300MP " , '<br>')
+
+    document.write("Battery LIfe: 40 Hours" , '<br>')
+
+    document.write("Screen Size: 7.7 inches" , '<br>')
+}
+
+
+
+ else {
+    document.write("Try Again !")
+}
+
